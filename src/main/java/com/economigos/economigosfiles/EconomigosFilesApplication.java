4ddd4644c7d62c73bd31e8514623a82c46449f66
VMarkz <vitor.portela@bandtec.com.br>
2021-05-10 19:02:55 -0300

    Create Project

---
+++ src/main/java/com/economigos/economigosfiles/EconomigosFilesApplication.java
@@ -0,0 +1,13 @@
+package com.economigos.economigosfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomigosFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomigosFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/economigos/economigosfiles/EconomigosFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.economigos.economigosfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomigosFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
