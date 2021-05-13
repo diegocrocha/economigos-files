9b7cc84eedc1cebf6d6901bf408fcf7a2dca550e
VMarkz <vitor.portela@bandtec.com.br>
2021-05-13 10:34:24 -0300

    Change the entire class to work with new logic(PilhaObj<ContabilUltimasAtividades>)

--- src/main/java/com/economigos/economigosfiles/utils/fileio/GravaArquivo.java
+++ src/main/java/com/economigos/economigosfiles/utils/fileio/GravaArquivo.java
@@ -1,109 +1,123 @@
 package com.economigos.economigosfiles.utils.fileio;
 
+import com.economigos.economigosfiles.models.ContabilUltimasAtividades;
+import com.economigos.economigosfiles.utils.structures.PilhaObj;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.FormatterClosedException;
+
 public class GravaArquivo {
 
-//    public void gravaPilha(PilhaObj<Renda> pilha, String nomeDoArquivo, Boolean csvFile) {
-//        FileWriter arquivo = null;
-//        String registro = "";
-//        String nomeDoArquivoDefault = "entregas.csv";
-//        boolean crashed = false;
-//
-//        if (nomeDoArquivo == null) {
-//            nomeDoArquivo = nomeDoArquivoDefault;
-//        }
-//        if (csvFile) {
-//            nomeDoArquivo += ".csv";
-//
-//            try {
-//                for (int i = 0; i < pilha.getSize(); i++) {
-//                    Renda rendaDaVez = pilha.pop();
-//                    registro = String.format
-//                            ("%04d ; %-20s ; %-30s ; %-50s ; %05d ; %05.2f;",
-//                                    rendaDaVez.getId(),
-//                                    rendaDaVez.getValor(),
-//                                    rendaDaVez.getRecebido(),
-//                                    rendaDaVez.getDescricao(),
-//                                    rendaDaVez.getDataPagamento()
-//                            );
-//                    gravaRegistro(nomeDoArquivo, registro);
-//                }
-//            } catch (FormatterClosedException erro) {
-//                System.err.println("Erro ao gravar no arquivo");
-//                crashed = true;
-//            } finally {
-//                try {
-//                    arquivo.close();
-//                } catch (IOException erro) {
-//                    System.err.println("Erro ao fechar arquivo.");
-//                    crashed = true;
-//                }
-//                if (crashed)
-//                    System.exit(1);
-//            }
-//
-//        } else {
-//            nomeDoArquivo += ".txt";
-//            gravarTxt(nomeDoArquivo, pilha);
-//        }
-//
-//    }
-//
-//    public static void gravaRegistro(String nomeArq, String registro) {
-//
-//        BufferedWriter saida = null;
-//        try {
-//            saida = new BufferedWriter(new FileWriter(nomeArq, true));
-//        } catch (IOException e) {
-//            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
-//        }
-//
-//        try {
-//            if(nomeArq.endsWith(".csv")){
-//                saida.append(registro);
-//                saida.close();
-//            }else{
-//                saida.append(registro + "\n");
-//                saida.close();
-//            }
-//
-//        } catch (IOException e) {
-//            System.err.printf("Erro ao gravar arquivo: %s.\n", e.getMessage());
-//        }
-//    }
-//
-//
-//    public static void gravarTxt(String nomeArq, PilhaObj<Entrega> entregas) {
-//
-//        String header = "";
-//        String corpo = "";
-//        String trailer = "";
-//        int contRegDados = 0;
-//
-//        Date dataDeHoje = new Date();
-//        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-//
-//        header += "00ENTREGAS00189236";
-//        header += formatter.format(dataDeHoje);
-//        header += "01";
-//        gravaRegistro(nomeArq, header);
-//
-//        for (int i = 0; i < entregas.getTamanho(); i++) {
-//            Entrega entrega = entregas.getElemento(i);
-//
-//            corpo += "02";
-//            corpo += String.format("%-5d", entrega.getId());
-//            corpo += String.format("%-20s", entrega.getEstado());
-//            corpo += String.format("%-30s", entrega.getCidade());
-//            corpo += String.format("%-50s", entrega.getRua());
-//            corpo += String.format("%05d", entrega.getNumero());
-//            corpo += String.format("%05.2f", entrega.getDistancia());
-//            contRegDados++;
-//            gravaRegistro(nomeArq, corpo);
-//        }
-//
-//        trailer += "01";
-//        trailer += String.format("%010d", contRegDados);
-//        gravaRegistro(nomeArq, trailer);
-//    }
+    private static File file;
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static void gravaRegistro(String nomeArq, String registro) {
+        BufferedWriter saida = null;
+
+        try {
+            saida = new BufferedWriter(new FileWriter(nomeArq, true));
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
+
+        try {
+                saida.append(registro + "\n");
+                saida.close();
+
+        } catch (IOException e) {
+            System.err.printf("Erro ao gravar arquivo: %s.\n", e.getMessage());
+        }
+    }
+
+    public static void gravaPilha(PilhaObj<ContabilUltimasAtividades> pilha, String nomeDoArquivo, Boolean csvFile) throws IOException {
+        String registro = "";
+        boolean crashed = false;
+
+        if (csvFile) {
+            nomeDoArquivo = "./src/main/resources/output-files/"+ nomeDoArquivo + ".csv";
+            file = new File(nomeDoArquivo);
+            FileWriter arquivo = new FileWriter(file);
+            arquivo.flush();
+
+            try {
+                for (int i = 0; i < pilha.getSize(); i++) {
+                    ContabilUltimasAtividades atividadeDaVez = pilha.pop();
+                    registro = String.format
+                            ("%-5s;%05.2f;%-30s;%s;",
+                                    atividadeDaVez.getTipo(),
+                                    atividadeDaVez.getValor(),
+                                    atividadeDaVez.getDescricao(),
+                                    atividadeDaVez.getData()
+                            );
+                    gravaRegistro(nomeDoArquivo, registro);
+                }
+            } catch (FormatterClosedException erro) {
+                System.err.println("Erro ao gravar no arquivo");
+                crashed = true;
+            } finally {
+                try {
+                    arquivo.close();
+                } catch (IOException erro) {
+                    System.err.println("Erro ao fechar arquivo.");
+                    crashed = true;
+                }
+                if (crashed)
+                    System.exit(1);
+            }
+
+        } else {
+            nomeDoArquivo = "./src/main/resources/output-files/"+ nomeDoArquivo + ".txt";
+            file = new File(nomeDoArquivo);
+            FileWriter arquivo = new FileWriter(file);
+            arquivo.flush();
+            gravarTxt(nomeDoArquivo, pilha);
+        }
+
+    }
+
+    public static void gravarTxt(String nomeArq, PilhaObj<ContabilUltimasAtividades> pilha) {
+
+        String header = "";
+        String corpo = "";
+        String trailer = "";
+        int contRegDados = 0;
+
+        Date dataDeHoje = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+        header += "00EXTRATO00189236";
+        header += formatter.format(dataDeHoje);
+        header += "01";
+        gravaRegistro(nomeArq, header);
+
+        for (int i = 0; i < pilha.getSize(); i++) {
+            ContabilUltimasAtividades atividadeDaVez = pilha.pop();
+
+            corpo = "";
+            if(atividadeDaVez.getTipo().equals("Gasto")){
+                corpo += "02";
+            }else {
+                corpo += "03";
+            }
+            corpo += String.format("%-5s", atividadeDaVez.getTipo());
+            corpo += String.format("%05.2f", atividadeDaVez.getValor());
+            corpo += String.format("%-30s", atividadeDaVez.getData());
+            corpo += String.format("%s", atividadeDaVez.getData());
+            contRegDados++;
+            gravaRegistro(nomeArq, corpo);
+        }
+
+        trailer += "01";
+        trailer += String.format("%010d", contRegDados);
+        gravaRegistro(nomeArq, trailer);
+    }
 
 }
