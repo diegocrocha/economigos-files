d761da6c15add082749efb049dd33b90c20ba927
VMarkz <vitor.portela@bandtec.com.br>
2021-05-21 22:27:37 -0300

    Ad ec2 url

--- src/main/java/com/economigos/economigosfiles/services/EconomigosService.java
+++ src/main/java/com/economigos/economigosfiles/services/EconomigosService.java
@@ -11,30 +11,43 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class EconomigosService {
 
+    public static String getBaseUrl(){
+        String baseUrl = "";
+
+        if (System.getenv("SERVICE_URL") != null){
+            baseUrl = System.getenv("SERVICE_URL");
+        }else{
+            baseUrl = "http://ec2-34-236-53-23.compute-1.amazonaws.com:8080/";
+        }
+
+        return baseUrl;
+    }
+
     public static UltimasAtividades requestContaById(Long idUsuario, Long idConta){
         RestTemplate restTemplate = new RestTemplate();
-        UltimasAtividades ultimasAtividades = restTemplate.getForObject( "http://localhost:8080" + "/economigos/contas/"+idConta+"/ultimas-atividades?idUsuario="+idUsuario, UltimasAtividades.class);
+        System.out.println(getBaseUrl());
+        UltimasAtividades ultimasAtividades = restTemplate.getForObject( getBaseUrl() + "/economigos/contas/"+idConta+"/ultimas-atividades?idUsuario="+idUsuario, UltimasAtividades.class);
 
         return ultimasAtividades;
     }
 
     public static ContaDto requestContaByApelido(String contaApelido){
         RestTemplate restTemplate = new RestTemplate();
-        ContaDto contaDto = restTemplate.getForObject( "http://localhost:8080" + "/economigos/contas/conta?apelido="+contaApelido.trim(), ContaDto.class);
+        ContaDto contaDto = restTemplate.getForObject( getBaseUrl() + "/economigos/contas/conta?apelido="+contaApelido.trim(), ContaDto.class);
 
         return contaDto;
     }
 
     public static CategoriaDto requestCategoriaByNome(String categoriaNome){
         RestTemplate restTemplate = new RestTemplate();
-        CategoriaDto categoriaDto = restTemplate.getForObject( "http://localhost:8080" + "/economigos/categorias/categoria?categoriaNome="+categoriaNome.trim(), CategoriaDto.class);
+        CategoriaDto categoriaDto = restTemplate.getForObject( getBaseUrl() + "/economigos/categorias/categoria?categoriaNome="+categoriaNome.trim(), CategoriaDto.class);
 
         return categoriaDto;
     }
 
     public static Boolean createRenda(RendaForm renda) {
         try {
-            new RestTemplate().postForObject("http://localhost:8080/economigos/rendas", renda, RendaForm.class);
+            new RestTemplate().postForObject(getBaseUrl() + "/economigos/rendas", renda, RendaForm.class);
             return true;
         } catch (Exception e) {
             return false;
@@ -43,7 +56,7 @@ public class EconomigosService {
 
     public static Boolean createGasto(GastoForm gasto) {
         try {
-            new RestTemplate().postForObject("http://localhost:8080/economigos/gastos", gasto, GastoForm.class);
+            new RestTemplate().postForObject(getBaseUrl() + "/economigos/gastos", gasto, GastoForm.class);
             return true;
         } catch (Exception e) {
             return false;
